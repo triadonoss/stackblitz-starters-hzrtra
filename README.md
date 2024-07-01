9fa68449312e6a3e6e668ad7f9bf18a921cb053d
triadonoss <85204936+triadonoss@users.noreply.github.com>
2024-07-02 05:22:06 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-hzrtra
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/triadonoss/stackblitz-starters-hzrtra)
---
+++ script.js
@@ -0,0 +1,11 @@
+function showMenu(menuId) {
+  // Hide all menus
+  const menus = document.querySelectorAll('#app > div');
+  menus.forEach((menu) => menu.classList.add('hidden'));
+
+  // Show the selected menu
+  const menu = document.getElementById(menuId);
+  if (menu) {
+    menu.classList.remove('hidden');
+  }
+}
