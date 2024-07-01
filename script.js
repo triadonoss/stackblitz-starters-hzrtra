3c3e792a12433857c2c795b7a2e45ccf8dd573f4
triadonoss <triadonoss@gmail.com>
2024-07-02 05:51:11 +0800

    update

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function showMenu(menuId) {
   // Hide all menus
   const menus = document.querySelectorAll('#app > div');
-  menus.forEach((menu) => menu.classList.add('hidden'));
+  menus.forEach(menu => menu.classList.add('hidden'));
 
   // Show the selected menu
   const menu = document.getElementById(menuId);
   if (menu) {
-    menu.classList.remove('hidden');
+      menu.classList.remove('hidden');
   }
 }
