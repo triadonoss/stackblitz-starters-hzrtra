d016eafd0bad501abe04e70677f84714f1b0d0c2
null <null>
2024-07-04 19:48:33 +0800

    updated the ui

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-function showMenu(menuId) {
-  // Hide all menus
-  const menus = document.querySelectorAll('#app > div');
-  menus.forEach(menu => menu.classList.add('hidden'));
+function selectOption(option) {
+  alert(option + ' selected');
+  document.querySelector('.options').classList.add('hidden');
+  document.getElementById('locations-pallet').classList.remove('hidden');
+  setTimeout(() => {
+      document.getElementById('locations-pallet').classList.add('hidden');
+      document.getElementById('require-rework').classList.remove('hidden');
+      document.querySelector('.rework-options').classList.remove('hidden');
+  }, 2000);
+}
 
-  // Show the selected menu
-  const menu = document.getElementById(menuId);
-  if (menu) {
-      menu.classList.remove('hidden');
+function rework(option) {
+  alert('Rework: ' + option);
+  document.getElementById('require-rework').classList.add('hidden');
+  document.querySelector('.rework-options').classList.add('hidden');
+  if (option === 'YES') {
+      alert('Returning to Starting Point');
+      document.getElementById('starting-point').scrollIntoView();
+      document.querySelector('.options').classList.remove('hidden');
+  } else {
+      document.getElementById('locations-drop').classList.remove('hidden');
+      setTimeout(() => {
+          document.getElementById('locations-drop').classList.add('hidden');
+          document.getElementById('flow-completed').classList.remove('hidden');
+          alert('Flow Completed');
+      }, 2000);
   }
 }
